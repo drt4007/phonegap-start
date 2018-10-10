e71007a573fefa54751505371af23bfc407d7877
divyanshu <divyanshu4007@gmail.com>
2018-10-10 19:16:18 +0530

    My Restaura

--- www/js/index.js
+++ www/js/index.js
@@ -1,74 +1,53 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-	document.getElementById('btnCalculate').addEventListener('click',this.onCalculate, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+    if ($.isWin) {
+
+        document.getElementById('themeStylesheet').setAttribute('href','./css/chui-win-3.9.2.css')
+    }
+    else if ($.isAndroid) {
+
+        document.getElementById('themeStylesheet').setAttribute('href', './css/chui-android-3.9.2.css')
+    }
+   else if ($.isiOS) {
+
+        document.getElementById('themeStylesheet').setAttribute('href', './css/chui-ios-3.9.2.css')
+    }
+    else {
+
+        document.getElementById('themeStylesheet').setAttribute('href', './css/chui-win-3.9.2.css')
+    }
+  
+		var tabbarOP={
+		tabs: 4,
+		icons:["home","drinks","cakes","pizzas"],
+		labels:["Home","Drinks","Cakes","Pizzas"],
+		selected: 1
+		};
+  document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+	$.UITabbar(tabbarOP);
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+    };
 
-        console.log('Received Event: ' + id);
-    },
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
 
-	onCalculate: function(){
-
-	var b= document.getElementById('leng').value;
-	var h= document.getElementById('bred').value;
- 	
-	if(b.length == 0|| h.length == 0)
-	document.getElementById('res').innerHTML="ERROR: Please fill all the fields";
-
-	else if(b == 0|| h == 0)
-	document.getElementById('res').innerHTML="ERROR: Please fill all the values";
-
-	else 	
-	{
-	var x=parseInt(b);
-	var y=parseInt(h);
-	var area = x*y;
-	document.getElementById('res').innerHTML = "Area="+area;
-	}
-
-
-	}
-
-
-};
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+} )();
